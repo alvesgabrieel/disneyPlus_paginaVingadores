45a34354f9337595d06dffaa1a210aaf6a9c1ebf
Gabriel Alves <gabriel_alvesqm@hotmail.com>
2023-01-24 20:41:27 -0300

    add js to carousel

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,13 +1,21 @@
 const carouselList = document.querySelector('[data-carousel="list"]');
 const carouselItem = document.querySelectorAll('[data-carousel="item"]');
-const btnPrevious = document.querySelector('[data-carousel="btn-previous"]');
-const btnNext = document.querySelector('[data-carousel="btn-next"]');
 
 const state = {
     mouseDownPosition: 0,
     movement: 0,
     lastTranslatePosition: 0,
-    currentSlidePosition: 0
+    currentSlidePosition: 0,
+    currentItemIndex: 0,
+    currentSlideIndex: 0
+}
+
+function animateTransition(active) {
+    if(active) {
+        carouselList.style.transition = 'transform .3s'
+    } else {
+        carouselList.style.removeProperty('transition')
+    }
 }
 
 function preventDefault(event){
@@ -19,31 +27,71 @@ function translateSlide(position){
     carouselList.style.transform = `translateX(${position}px)`;
 }
 
-function onMouseUp(event) {
-    const item = event.currentTarget;
-    item.removeEventListener('mousemove', onMouseMove);
-    console.log('mouse up');
+function getCenterPosition(slideIndex) {
+    const item = carouselItem[state.currentItemIndex];
+    const itemWidth = item.offsetWidth;
+    const bodyWidth = document.body.clientWidth;
+    const slideWidth = itemWidth * 5;
+    const margin = (bodyWidth - slideWidth) / 2;
+    return margin - (slideWidth * slideIndex);
 }
 
-function onMouseDown(event) {
+function setVisibleSlide(slideIndex) {
+    state.currentSlideIndex = slideIndex;
+    const centerPosition = getCenterPosition(slideIndex);
+    animateTransition(true);
+    translateSlide(centerPosition);
+}
+
+function backwardSlide() {
+    if(state.currentSlideIndex > 0) {
+        setVisibleSlide(state.currentSlideIndex - 1);
+    } else {
+        setVisibleSlide(state.currentSlideIndex);
+    }
+}
+
+function forwardSlide() {
+    const lastItemIndex = carouselItem.length - 1;
+    const lastSlideIndex = Math.floor(lastItemIndex / 5)
+    if(state.currentSlideIndex < lastSlideIndex) {
+        setVisibleSlide(state.currentSlideIndex + 1);
+    } else {
+        setVisibleSlide(state.currentSlideIndex);
+    }
+}
+
+function onMouseUp(event) {
+    if(state.movement > 150) {
+        backwardSlide();
+    } else if (state.movement < -150) {
+        forwardSlide();
+    } else {
+        setVisibleSlide(state.currentSlideIndex);
+    }
+
     const item = event.currentTarget;
+    item.removeEventListener('mousemove', onMouseMove);
+}
+
+function onMouseDown(event, index) {
+    const item = event.currentTarget;
+    state.currentItemIndex = index;
     state.mouseDownPosition = event.clientX;
     state.currentSlidePosition = event.clientX - state.lastTranslatePosition;
+    animateTransition(false);
     item.addEventListener('mousemove', onMouseMove);
-    console.log('mouse down');
 }
 
 function onMouseMove(event) {
     state.movement = event.clientX - state.mouseDownPosition;
     const position = event.clientX - state.currentSlidePosition;
     translateSlide(position);
-    console.log('mouse move');
 }
 
 function onMouseLeave(event) {
     const item = event.currentTarget;
     item.removeEventListener('mousemove', onMouseMove);
-    console.log('mouse leave');
 }
 
 function setListeners() {
@@ -52,9 +100,13 @@ function setListeners() {
         link.addEventListener('click', preventDefault);
         item.addEventListener('dragstart', preventDefault);
         item.addEventListener('mouseup', onMouseUp);
-        item.addEventListener('mousedown', onMouseDown);
+        item.addEventListener('mousedown', function(event) {
+            onMouseDown(event, index);
+        });
         item.addEventListener('mouseleave', onMouseLeave);
     })
 }
 
 setListeners();
+setVisibleSlide(0)
+
